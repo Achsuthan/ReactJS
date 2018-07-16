722a84aec5c6c04af1a5ed608d38e379adc07f6b
Achsuthan <achsuthan@pikanite.com>
2018-07-16 20:39:36 +0530

    constructor added to react.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,9 +5,23 @@ import Intro from './Intro';
 import './App.css';
 
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.firstname = "Mahendran"
+    this.state = {}
+  }
+
+  setName(name){
+    this.lastname = name
+    return this.lastname
+  }
+
+
   render() {
     return (
       <div className="App">
+      <h1>Hello User : {this.firstname} {this.setName("Achsuthan")}</h1>
       <Header />
       <Intro />
       </div>
