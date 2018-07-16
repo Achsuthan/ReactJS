d3f13000eb5e2636a40a5dd5fdf1506d89afafb7
Achsuthan <achsuthan@pikanite.com>
2018-07-16 22:30:10 +0530

    propTypes addded

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './Header';
 import Intro from './Intro';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
       <h1>Hello User : {this.firstname} {this.setName("Achsuthan")}</h1>
+      <h2>{this.props.id} {this.props.name} {this.props.stable}</h2>
       <Header msg = {this.props.id}/>
       <Intro />
       </div>
@@ -34,9 +36,18 @@ class App extends Component {
   }
 }
 
+//define the type of props type 
+App.propTypes = {
+  id :  PropTypes.string,
+  name :  PropTypes.string,
+  stable :  PropTypes.bool,
+}
+
 //default props for this App component
 App.defaultProps = {
-    id : "12342"
+    id : "12",
+    name : "Achsuthan",
+    stable : true
 }
 
 
