1982a77b4b1173abb70944bf38c7b9938795b2e5
Achsuthan <achsuthan@icloud.com>
2019-10-30 10:39:36 +0530

    1. Button event added
    >>> ach

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,24 +2,25 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
-
-  renderTag() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
+  render() {
     return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li>{tag}</li>
-        ))}
-      </ul>
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
-  render() {
-    return <div>{this.renderTag()}</div>;
-  }
+
+  handleIncrement = () => {
+    console.log("This ", this);
+  };
 
   getBadgeClass() {
     let clases = "badge m-2 badge-";
