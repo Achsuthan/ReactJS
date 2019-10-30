2d9c6af5021c6d587c55a26c705504c4333b632f
Achsuthan <achsuthan@icloud.com>
2019-10-30 09:28:02 +0530

    1. Dyanamic class added
    >>> ach

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -4,20 +4,19 @@ class Counter extends Component {
   state = {
     count: 0
   };
-
-  styles = {
-    fontSize: 10,
-    fontWeight: "Bold"
-  };
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClass() {
+    let clases = "badge m-2 badge-";
+    clases += this.state.count === 0 ? "warning" : "primary";
+    return clases;
   }
 
   formatCount() {
