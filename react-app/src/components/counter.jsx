5392d0b4fee767af0fe3fde1c578caa60f031c60
Achsuthan <achsuthan@icloud.com>
2019-10-30 09:45:04 +0530

    1. Condition render added
    >>> ach

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -5,18 +5,20 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
-  render() {
+
+  renderTag() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
     return (
-      <div>
-        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li>{tag}</li>
+        ))}
+      </ul>
     );
+  }
+  render() {
+    return <div>{this.renderTag()}</div>;
   }
 
   getBadgeClass() {
