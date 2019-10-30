78b951548c7d1dbfeb1ca751f94f2e76ae72cb47
Achsuthan <achsuthan@icloud.com>
2019-10-30 10:47:45 +0530

    1. Function added with argument
    >>> ach

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -9,7 +9,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(2)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -18,7 +18,7 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
+  handleIncrement = val => {
     console.log("This ", this);
     this.setState({
       count: this.state.count + 1
